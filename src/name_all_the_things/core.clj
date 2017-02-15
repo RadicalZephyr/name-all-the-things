a3a5731d304f3f1e476ec27a3b63e3fe9dd12c5b
Geoff Shannon <geoffpshannon@gmail.com>
2017-02-15 02:04:59 -0600

    Add generating the products of many different groups

--- src/name_all_the_things/core.clj
+++ src/name_all_the_things/core.clj
@@ -6,3 +6,17 @@
           (for [x a
                 y b]
             [[x y] [y x]]))))
+
+(defn simple-self-cartesian-product [coll]
+  (set
+   (for [a coll
+         b coll
+         :when (not= a b)]
+     [a b])))
+
+(defn all-cartesian-products
+  ([coll] (simple-self-cartesian-product coll))
+  ([coll & groups]
+   (set
+    (mapcat #(apply cartesian-product %)
+            (simple-self-cartesian-product (conj groups coll))))))
--- test/name_all_the_things/core_test.clj
+++ test/name_all_the_things/core_test.clj
@@ -11,3 +11,13 @@
 
   (t/is (= #{[1 2] [2 1] [1 3] [3 1]}
            (sut/cartesian-product #{3 2} #{1}))))
+
+(t/deftest test-all-cartesian-products
+  (t/is (= #{[1 :a] [:a 1] [1 'c] ['c 1] [:a 'c] ['c :a]}
+           (sut/all-cartesian-products #{1}
+                                       #{:a}
+                                       #{'c})))
+
+  (t/is (= #{["abc" "def"]
+             ["def" "abc"]}
+           (sut/all-cartesian-products #{"abc" "def"}))))
