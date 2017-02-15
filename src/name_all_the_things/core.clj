00d12e48aecb6aa17808fd1e276d88cb8e7d2540
Geoff Shannon <geoffpshannon@gmail.com>
2017-02-15 04:16:06 -0600

    Completely rework algorithm based on notes
    - Use combinatorics library code more heavily to simplify
    - Correctly filter any tuples with duplicate values after generating
      everything

--- src/name_all_the_things/core.clj
+++ src/name_all_the_things/core.clj
@@ -1,59 +1,20 @@
 (ns name-all-the-things.core
-  (:require [clojure.math.combinatorics :refer [combinations]]))
+  (:require [clojure.math.combinatorics :refer [cartesian-product combinations]]))
 
-(defn all-pairs-equality-checks [item-syms]
-  `(and
-    ~@(map (fn [[x y]] `(not= ~x ~y)) (combinations item-syms 2))))
+(defn any-duplicates? [tuple]
+  (boolean
+   (some #(apply = %)
+         (combinations tuple 2))))
 
-(defmacro cartesian-product* [include-dups? & colls]
-  (let [item-syms (repeatedly (count colls) #(gensym "item"))]
-    `(set
-      (for ~(vec (concat
-                  (interleave item-syms colls)
-                  (when-not include-dups?
-                    [:when (all-pairs-equality-checks item-syms)])))
-        ~(vec item-syms)))))
+(defn name-tuples
+  "Generate n-tuples from the given collections.
 
-(defn cartesian-product
-  ([a b]
-   (cartesian-product* false a b))
-  ([a b c]
-   (cartesian-product* false a b c)))
-
-(defn all-cartesian-product-pairs
-  "Generate the set of all cartesian products of the given sets.
-
-  The Cartesian square of the first set is included in the set of
-  pairs returned. The remaining
-  "
-  [coll & groups]
-  (let [all-groups (conj groups coll)]
+  Multiple items from the first collection can appear in the same
+  tuple. All following collections will contribute at most one item to
+  any tuple."
+  [n main-group & groups]
+  (let [all-groups (concat groups (map hash-set (set main-group)))]
     (set
-     (->> (cartesian-product all-groups all-groups)
+     (->> (combinations all-groups n)
           (mapcat #(apply cartesian-product %))
-          (concat (cartesian-product coll coll))))))
-
-(defn all-cartesian-product-triples
-  "Generate the set of all cartesian products of the given sets.
-
-  The Cartesian square of the first set is included in the set of
-  pairs returned. The remaining
-  "
-  [coll & groups]
-  (let [all-groups (conj groups coll)]
-    (set
-     (->> (cartesian-product all-groups all-groups all-groups)
-          (mapcat #(apply cartesian-product %))
-          (concat (cartesian-product* true coll coll coll))))))
-;; Probably the solution for avoiding dups is to do it here at the
-;; end.  So instead of using the :when filter in the cartesian product
-;; for generator, post-filter for none of the pairs being equal
-
-;; Also it's not actualy cartesian product that should be selecting
-;; what to call cartesian product on. It should be combinations again,
-;; and then the way we get choose 2 or 3 words is to take 2 or 3 size
-;; combinations.
-
-;; Also, the way to deal with initial collection being different is to
-;; add it to the group by splitting each item inside it into an
-;; individual set.
+          (remove any-duplicates?)))))
--- test/name_all_the_things/core_test.clj
+++ test/name_all_the_things/core_test.clj
@@ -2,74 +2,66 @@
   (:require [clojure.test :as t]
             [name-all-the-things.core :as sut]))
 
-(t/deftest test-all-pairs-equality-checks
-  (t/is (= '(clojure.core/and)
-           (sut/all-pairs-equality-checks [])))
+(t/deftest test-any-duplicates
+  (t/is (= true
+           (sut/any-duplicates? [1 1])))
+  (t/is (= true
+           (sut/any-duplicates? [2 1 1])))
+  (t/is (= true
+           (sut/any-duplicates? [1 2 1])))
 
-  (t/is (= '(clojure.core/and)
-           (sut/all-pairs-equality-checks ['a])))
 
-  (t/is (= '(clojure.core/and (clojure.core/not= a b))
-           (sut/all-pairs-equality-checks ['a 'b])))
+  (t/is (= false
+           (sut/any-duplicates? [1 2])))
+  (t/is (= false
+           (sut/any-duplicates? [1 2 3]))))
 
-  (t/is (= '(clojure.core/and (clojure.core/not= a b)
-                              (clojure.core/not= a c)
-                              (clojure.core/not= b c))
-           (sut/all-pairs-equality-checks ['a 'b 'c]))))
+(t/deftest test-name-tuples
+  (t/testing "n == 2"
+    (t/is (= #{[1  'c] [:a 'c] [ 1 :a]}
+             (sut/name-tuples 2
+                              #{}
+                              #{1}
+                              #{:a}
+                              #{'c})))
 
-(t/deftest test-all-cartesian-product-pairs
-  (t/is (= #{[1  'c] ['c  1]
-             [:a 'c] ['c :a]
-             [:a  1] [ 1 :a]}
-           (sut/all-cartesian-product-pairs #{}
-                                            #{1}
-                                            #{:a}
-                                            #{'c})))
+    (t/is (= #{["abc" "def"]}
+             (sut/name-tuples 2
+                              #{"abc" "def"})))
 
-  (t/is (= #{["def" "abc"] ["abc" "def"]}
-           (sut/all-cartesian-product-pairs #{"abc" "def"})))
+    (t/is (= #{["abc" "def"] [1 "def"] [1 "abc"]}
+             (sut/name-tuples 2
+                              #{"abc" "def"}
+                              #{1})))
 
-  (t/is (= #{["def" "abc"] ["abc" "def"]
-             ["def" 1] [1 "def"]
-             ["abc" 1] [1 "abc"]}
-           (sut/all-cartesian-product-pairs #{"abc" "def"}
-                                            #{1}))))
+    (t/is (= #{["abc" "def"]}
+             (sut/name-tuples 2
+                              ["abc" "def" "abc"])))
 
-(t/deftest test-all-cartesian-product-triples
-  (t/is (= #{[1  :a 'c]
-             [1  'c :a]
-             ['c  1 :a]
-             ['c  :a 1]
-             [:a  'c 1]
-             [:a  1 'c]}
-           (sut/all-cartesian-product-triples #{}
-                                              #{1}
-                                              #{:a}
-                                              #{'c})))
+    (t/is (= #{["abc" "def"]}
+             (sut/name-tuples 2
+                              #{"abc" "def"}
+                              ["abc"]))))
 
-  (t/is (= #{}
-           (sut/all-cartesian-product-triples #{"abc" "def"})))
+  (t/testing "n == 3"
+    (t/is (= #{[1  :a 'c]}
+             (sut/name-tuples 3
+                              #{}
+                              #{1}
+                              #{:a}
+                              #{'c})))
 
-  (t/is (= #{[1 "def" "abc"]
-             [1 "abc" "def"]
-             ["abc" 1 "def"]
-             ["abc" "def" 1]
-             ["def" "abc" 1]
-             ["def" 1 "abc"]}
-           (sut/all-cartesian-product-triples #{"abc" "def"}
-                                              #{1})))
+    (t/is (= #{}
+             (sut/name-tuples 3
+                              #{"abc" "def"})))
 
-  (t/is (= #{[1 "def" "abc"]
-             [1 "abc" "def"]
-             ["abc" 1 "def"]
-             ["abc" "def" 1]
-             ["def" "abc" 1]
-             ["def" 1 "abc"]
-             [2 "def" "abc"]
-             [2 "abc" "def"]
-             ["abc" 2 "def"]
-             ["abc" "def" 2]
-             ["def" "abc" 2]
-             ["def" 2 "abc"]}
-           (sut/all-cartesian-product-triples #{"abc" "def"}
-                                              #{1 2}))))
+    (t/is (= #{[1 "abc" "def"]}
+             (sut/name-tuples 3
+                              #{"abc" "def"}
+                              #{1})))
+
+    (t/is (= #{[1 "abc" "def"]
+               [2 "abc" "def"]}
+             (sut/name-tuples 3
+                              #{"abc" "def"}
+                              #{1 2})))))
