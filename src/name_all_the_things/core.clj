004e0cc252b664793c8c37cb196ffd13eda6bd87
Geoff Shannon <geoffpshannon@gmail.com>
2017-02-15 01:52:18 -0600

    Add an implementation of a simple two-set cartesian product

--- src/name_all_the_things/core.clj
+++ src/name_all_the_things/core.clj
@@ -1,3 +1,8 @@
 (ns name-all-the-things.core)
 
-(defn cartesian-product [a b])
+(defn cartesian-product [a b]
+  (set
+   (apply concat
+          (for [x a
+                y b]
+            [[x y] [y x]]))))
--- test/name_all_the_things/core_test.clj
+++ test/name_all_the_things/core_test.clj
@@ -3,5 +3,11 @@
             [name-all-the-things.core :as sut]))
 
 (t/deftest test-cartesian-product
+  (t/is (= #{[2 2]}
+           (sut/cartesian-product #{2} #{2})))
+
   (t/is (= #{[1 2] [2 1]}
-           (sut/cartesian-product #{1} #{2}))))
+           (sut/cartesian-product #{1} #{2})))
+
+  (t/is (= #{[1 2] [2 1] [1 3] [3 1]}
+           (sut/cartesian-product #{3 2} #{1}))))
